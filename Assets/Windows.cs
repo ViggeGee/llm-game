026bc11155b9db9274f8ae4668c4490ba67bad00
ViggeGee <victorgustafsson65@gmail.com>
2025-03-16 01:09:01 +0100

    pickable items!

--- Assets/Windows.cs
+++ Assets/Windows.cs
@@ -4,6 +4,7 @@ public class Windows : MonoBehaviour
 {
     [SerializeField] GameObject profiles;
     [SerializeField] GameObject windows;
+    [SerializeField] GameObject shop;
     [SerializeField] GameObject playerCamera;
     [SerializeField] GameObject PcCamera;
  
@@ -13,10 +14,21 @@ public class Windows : MonoBehaviour
         windows.SetActive(false);
         profiles.SetActive(true);   
     }
+    public void OpenShop()
+    {
+        windows.SetActive(false);
+        shop.SetActive(true);   
+    }
     public void CloseProfiles()
     {
         windows.SetActive(true);
         profiles?.SetActive(false);
+    }
+
+    public void CloseShop()
+    {
+        windows.SetActive(true);
+        shop.SetActive(false);
     }
 
     public void TurnOffPc()
